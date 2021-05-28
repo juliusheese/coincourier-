bd012555e33b27fde9b9bb1c95cbc831ac33e997
Julius Shapiro <juliusshapiro@Juliuss-MBP.localdomain>
2021-05-28 09:50:56 -0500

    𐌰

--- src/App.js
+++ src/App.js
@@ -13,13 +13,16 @@ function App() {
         <Text fontSize="0.69420rem"h="100vh"m={["2.69420%","2.69420%","2.69420%","2.69420%"]} color="#ffffff" >
           We are a 𐌱itcoin Guild with an on the ground emphasis to improving the protocol.
           Our services include a 2 hour delivery service of a device that could be blank or not.
-          Our services are in Cook, Lake, and Champaign county in Illinois, USA.
-          We are also building a proprietary BTC ATM device.
+          We also have metal coin wallet delivery.
+          Our services are in Cook, Lake, and Champaign County in Illinois, US.
+          We are also building a proprietary 𐌱TC ATM device.
           We plan to expand to more locations soon. Parties who are interested in joining the team should email:<Center><Text  color="purple" ><a href="mailto: initiation@bitcoinwallet.delivery"> initiation@bitcoinwallet.delivery </a></Text></Center>
-          We are headquartered in Chicago, Des Moines {<ReactCountryFlag countryCode="us" />} and Gielinor and have additional headquarters in Estonia {<ReactCountryFlag countryCode="ee" />}. We hold an Estonian activity license for a provider of
-          Virtual Currency Services (VCS {<Icon color="#f2a900" as={FaBitcoin} ></Icon>} ) and follow all rules under the Money Laundering and Terrorist Financing Prevention Act.  
-          We prefer emails with keys so make sure to set things up properly.
-          日本のクライアント：私達に電子メールを送ってください ！نحن نحب العملاء الدوليين  
+          We are headquartered in Chicago && Des Moines {<ReactCountryFlag countryCode="us" />} and have technical support in Estonia {<ReactCountryFlag countryCode="ee" />}. We hold an Estonian activity license for a provider of
+          Virtual Currency Services (VCS {<Icon color="#f2a900" as={FaBitcoin} ></Icon>} ) once we are out of beta and follow all rules under the Money Laundering and Terrorist Financing Prevention Act.  
+          We prefer emails with keys so make sure to follow the protocol.
+          日本のクライアント：私達に電子メールを送ってください ！نحن نحب العملاء الدوليين.
+          <br></br>
+          Ask us about special metal coin wallet delivery‼️
         </Text>
         </Box>
       </Center>
