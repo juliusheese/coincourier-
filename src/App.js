46eb207b4ae96fba37a380d9b4624fca6d69f264
Julius Shapiro <juliusshapiro@Juliuss-MBP.localdomain>
2021-06-02 09:14:41 -0500

    AAAA

--- src/App.js
+++ src/App.js
@@ -7,22 +7,21 @@ function App() {
   return (
     <div className="App">
     <Box bgGradient="linear(to-bl, #f2a900, #4d4d4e)">
-        <Box w="100vw"h="10vh"color="#ffffff"m={["1.69420%","1.69420%","1.69420%","1.69420%"]} ><Text fontSize="1.69420rem"> <Center>IN COLLEGIUM 𐌱itcoinwallet.delivery{<Icon color="#f2a900"as={FaBitcoin}></Icon>}</Center></Text></Box>
+        <Box w="100vw"h="10vh"color="#ffffff"m={["1.69420%","1.69420%","1.69420%","1.69420%"]} ><Text fontSize="1.69420rem"> <Center>IN COLLEGIUM{<Icon color="#f2a900"as={FaBitcoin}></Icon>}</Center></Text></Box>
         <Center> 
         <Box>
         <Text fontSize="0.69420rem"h="100vh"m={["2.69420%","2.69420%","2.69420%","2.69420%"]} color="#ffffff" >
-          We are a 𐌱itcoin Guild with an on the ground emphasis to improving the protocol.
-          Our services include a 2 hour delivery service of a device that could be blank or not.
-          We also have metal coin wallet delivery.
-          Our services are in Cook, Lake, Springfield, and Champaign County in Illinois, US.
-          We are also building a proprietary 𐌱TC ATM device.
+          We are a Guild with an on the ground emphasis to improving the protocol.
+          Our services include a 1 hour delivery service of a device that could be blank or not.
+          Ask us about metal coin wallet delivery.
+          Our services are in Cook, Lake, Sangamon, and Champaign County in Illinois, US.
+          We are also building a proprietary ATM device.
           We plan to expand to more locations soon. Parties who are interested in joining the team should email:<Center><Text  color="purple" ><a href="mailto: initiation@bitcoinwallet.delivery"> initiation@bitcoinwallet.delivery </a></Text></Center>
-          We are headquartered in Chicago && Des Moines {<ReactCountryFlag countryCode="us" />} and have technical support in Estonia {<ReactCountryFlag countryCode="ee" />}. We hold an Estonian activity license for a provider of
+          We are headquartered in Chicago : Des Moines {<ReactCountryFlag countryCode="us" />} and have technical support in Estonia {<ReactCountryFlag countryCode="ee" />}. We hold an Estonian activity license for a provider of
           Virtual Currency Services (VCS {<Icon color="#f2a900" as={FaBitcoin} ></Icon>} ) once we are out of beta and follow all rules under the Money Laundering and Terrorist Financing Prevention Act.  
-          We prefer emails with keys so make sure to follow the protocol.
-          日本のクライアント：私達に電子メールを送ってください ！نحن نحب العملاء الدوليين.
+          We prefer emails with keys so make sure to follow the protocol
+          .日本のクライアント：私達に電子メールを送ってください！نحن نحب العملاء الدوليين.
           <br></br>
-          Momento Mori: Bit coin means Byte coin!
         </Text>
         </Box>
       </Center>
