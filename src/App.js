bed5be63fac58ccd0e66fdae60b70526b70a6c5e
Julius Shapiro <juliusshapiro@Juliuss-MBP.localdomain>
2021-06-24 17:41:04 -0500

    a

--- src/App.js
+++ src/App.js
@@ -12,15 +12,22 @@ function App() {
         <Box>
         <Text fontSize="0.69420rem"h="100vh"m={["2.69420%","2.69420%","2.69420%","2.69420%"]} color="#ffffff" >
           We are a Guild with an on the ground emphasis to improving the protocol.
-          Our services include a 1 hour delivery service of a device that could be blank or not.
-          Ask us about metal coin wallet delivery.
-          Our services are in Cook, Lake, Sangamon, and Champaign County in Illinois, US.
-          We are also building a proprietary ATM device.
-          We plan to expand to more locations soon. Parties who are interested in joining the team should email:<Center><Text  color="purple" ><a href="mailto: initiation@bitcoinwallet.delivery"> initiation@bitcoinwallet.delivery </a></Text></Center>
-          We are headquartered in Chicago : Des Moines {<ReactCountryFlag countryCode="us" />} and have technical support in Estonia {<ReactCountryFlag countryCode="ee" />}. We hold an Estonian activity license for a provider of
-          Virtual Currency Services (VCS {<Icon color="#f2a900" as={FaBitcoin} ></Icon>} ) once we are out of beta and follow all rules under the Money Laundering and Terrorist Financing Prevention Act.  
-          We prefer emails with keys so make sure to follow the protocol
-          .日本のクライアント：私達に電子メールを送ってください！نحن نحب العملاء الدوليين.
+          Our services include a 1 hour delivery service of an audio disc.
+          WE are availible in the north shore of Chicago.
+          We plan to expand to more locations soon like Sangamon, and Champaign County Illinois.
+          Parties who are interested in recieving audio CDs or joining the team should email <Center><Text  color="purple" ><a href="mailto: JJAutomata@protonmail.ch">us</a></Text></Center>
+          We are headquartered in Glencoe {<ReactCountryFlag countryCode="us" />} but have friends in Japan {<ReactCountryFlag countryCode="jp" />} :)
+          We prefer emails with keys so make sure to follow the protocol ...
+          We are also building a proprietary ATM just for kicks.
+          Our audio discs can be delivered using Uber parcel, but if you want insurance on your CD we can provide a private driver with insurance for a fee.
+          Since we are still in beta, we only offer delivery of discs with under 420,000 satoshi.
+          We run technology using the lightning network and
+          <Center><Text  color="purple" ><a href="getumbrel.com">Umbrel</a></Text></Center>
+          to deliver funds.
+          Further instructions on how to access your funds will be provided for on the audio disc.
+          Ask us about metal coin delivery!
+          <br></br>
+          bitcoin.delivery is not a money transmitter because we are delivering property
           <br></br>
         </Text>
         </Box>
