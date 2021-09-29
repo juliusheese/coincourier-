06ec1f6164b562debc2cf534f70524d1f518b011
Julius Shapiro <julius.42.shapiro@gmail.com>
2021-09-29 17:20:20 -0500

    new work

--- src/App.js
+++ src/App.js
@@ -7,27 +7,14 @@ function App() {
   return (
     <div className="App">
     <Box bgGradient="linear(to-bl, #f2a900, #4d4d4e)">
-        <Box w="100vw"h="10vh"color="#ffffff"m={["1.69420%","1.69420%","1.69420%","1.69420%"]} ><Text fontSize="1.69420rem"> <Center>IN COLLEGIUM{<Icon color="#f2a900"as={FaBitcoin}></Icon>}</Center></Text></Box>
+        <Box w="100vw"h="10vh"color="#ffffff"m={["1.69420%","1.69420%","1.69420%","1.69420%"]} ><Text fontSize="1.69420rem"> <Center>Julius H.G J AI LLC</Center></Text></Box>
         <Center> 
         <Box>
         <Text fontSize="0.69420rem"h="100vh"m={["2.69420%","2.69420%","2.69420%","2.69420%"]} color="#ffffff" >
-          We are a Guild with an on the ground emphasis to improving the protocol.
-          Our services include a 1 hour delivery service of an audio disc.
-          WE are availible in the north shore of Chicago.
-          We plan to expand to more locations soon like Sangamon, and Champaign County Illinois.
-          Parties who are interested in recieving audio CDs or joining the team should email <Center><Text  color="purple" ><a href="mailto: JJAutomata@protonmail.ch">us</a></Text></Center>
-          We are headquartered in Glencoe {<ReactCountryFlag countryCode="us" />} but have friends in Japan {<ReactCountryFlag countryCode="jp" />} :)
-          We prefer emails with keys so make sure to follow the protocol ...
-          We are also building a proprietary ATM just for kicks.
-          Our audio discs can be delivered using Uber parcel, but if you want insurance on your CD we can provide a private driver with insurance for a fee.
-          Since we are still in beta, we only offer delivery of discs with under 420,000 satoshi.
-          We run technology using the lightning network and
-          <Center><Text  color="purple" ><a href="getumbrel.com">Umbrel</a></Text></Center>
-          to deliver funds.
-          Further instructions on how to access your funds will be provided for on the audio disc.
-          Ask us about metal coin delivery!
-          <br></br>
-          bitcoin.delivery is not a money transmitter because we are delivering property
+          We are a online software solutions company, availible to contract for products.
+          Most of our previous projects use Javascript or ASP.NET , but we are able to solve, consult, or build out most web based projects and software.
+          Parties who are interested should email <Center><Text  color="purple" ><a href="mailto: JJAutomata@protonmail.ch">us</a></Text></Center>
+          We are headquartered in Chicago {<ReactCountryFlag countryCode="us" />} and have offices in Tokyo {<ReactCountryFlag countryCode="jp" />} :)
           <br></br>
         </Text>
         </Box>
