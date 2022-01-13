c18b5ecd4519692077b4fd39be3698430b07715c
johnprism <johnprism@gmail.com>
2022-01-13 06:58:29 -0800

    update

--- src/components/section/Contact.js
+++ src/components/section/Contact.js
@@ -28,13 +28,13 @@ const Contact = () => {
                         <a href="mailto:JJautomata@protonmail.ch">here.</a>
                     </p>
                 <div className="d-flex justify-content-center align-items-center flex-column">
-                <div className="inline-block">
+                {/* <div className="inline-block">
                     {social.linkedin && <a title="Visit Linkedin profile" rel="noopener noreferrer" target="_blank"  href={social.linkedin}><i className="fab fa-linkedin"></i></a>}
                     {social.facebook && <a title="Visit Facebok profile" rel="noopener noreferrer" target="_blank" href={social.facebook}><i className="fab fa-facebook"></i></a>}
                     {social.twitter && <a title="Visit Twitter profile" rel="noopener noreferrer" target="_blank" href={social.twitter}><i className="fab fa-twitter"></i></a>}
                     {social.instagram && <a title="Visit Instagram profile" rel="noopener noreferrer" target="_blank" href={social.instagram}><i className="fab fa-instagram"></i></a>}
                     {social.github && <a title="Visit Github profile" rel="noopener noreferrer" target="_blank" href={social.github}><i className="fab fa-github"></i></a>}<br/>
-                </div>
+                </div> */}
                     {social.resume && <a title="Download Resume" href={social.resume} download><i className="fas fa-download"></i></a>}
                 </div>
                 </div>
