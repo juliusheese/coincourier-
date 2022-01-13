21caa18ccb0fcdcc328fcfa2e63fba2a89c525bb
johnprism <johnprism@gmail.com>
2022-01-12 19:05:08 -0800

    new company website

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/pages/Home';
-import Services from './components/pages/Services';
-import SignUp from './components/pages/SignUp';
-import Products from './components/pages/Products';
-
-
+import React, { useEffect } from "react";
+import Particles from "./components/layouts/Particles";
+import Header from "./components/section/Header";
+import About from "./components/section/About";
+import Works from "./components/section/Works";
+import Contact from "./components/section/Contact";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { animation } from "./profile";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: animation.duration,
+      once: animation.once,
+      disable: !animation.animate,
+    });
+    // eslint-disable-next-line
+  }, []);
+
   return (
-   <>
-   <Router>
-      <Navbar />
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/services' exact component={Services} />
-        <Route path='/products' exact component={Products} />
-        <Route path='/sign-up' exact component={SignUp} />
-      </Switch>
-   </Router>
-   </>
+    <div className="App">
+      <Header />
+      <Particles />
+      <About />
+      <Works />
+      <Contact />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles/style.css'
+import './styles/images.css'
+import './styles/no-touch.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
